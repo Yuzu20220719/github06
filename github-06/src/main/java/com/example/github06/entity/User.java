c087b17ccb84727c841f73c677cb996edf68abc2
Yuzu20220719 <shirley-7@outlook.com>
2022-08-12 18:43:13 +0800

    forth commit : add user.java

---
+++ github-06/src/main/java/com/example/github06/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.github06.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    private String addr;
+    private String info;
+}
