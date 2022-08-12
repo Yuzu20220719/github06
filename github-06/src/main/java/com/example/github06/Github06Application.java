5dcd1b344665d997af6d1d87c1447f8d649a0df5
Yuzu20220719 <shirley-7@outlook.com>
2022-08-12 18:12:50 +0800

    second commit

---
+++ github-06/src/main/java/com/example/github06/Github06Application.java
@@ -0,0 +1,13 @@
+package com.example.github06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github06Application.class, args);
+    }
+
+}
---
+++ github-06/src/test/java/com/example/github06/Github06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Github06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
